45a4526f613ee016ee01dae247815524248bc069
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2023-03-31 17:27:03 -0300

    Update elevador.sv

--- R01/problema01/elevador.sv
+++ R01/problema01/elevador.sv
@@ -9,6 +9,10 @@ module elevador(
 	output logic P
 );
   
+  // Check if the door is open or closed
+  // the door is open iff the elevator is
+  // not moving (M is False) and the elevator
+  // is in one of the floors (either A1, A2 or A3)
   always_comb P <= ~M & (A1 | A2 | A3);
 
 endmodule
