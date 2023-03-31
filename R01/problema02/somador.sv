83e5dad87c5d01b0b55ce4c55b38f781fd6c73ad
Alexandre Corlet <alexandre.santos@ccc.ufcg.edu.br>
2023-03-31 09:11:43 -0300

    Update somador.sv

--- R01/problema02/somador.sv
+++ R01/problema02/somador.sv
@@ -4,18 +4,18 @@
  * (c) Alexandre B. Corlet, 119210883
  */
 
-parameter NUM_BITS = 8;
+parameter NUM_BITS = 8;		// Define a constant which represent the maximum number of bits
 
 module somador(
-  input signed [NUM_BITS-1:0] A, B,
-  output signed [NUM_BITS-1:0] S,
+  input logic signed [NUM_BITS-1:0] A, B,
+  output logic signed [NUM_BITS-1:0] S,
   output logic Z, N, P
   );
   always_comb begin
     // Compute sum of A and B and store it on S
     S <= A + B;
     // Check if S == 0, if S == 0, then Z := 1, otherwise set Z := 0
-    if (S == 0)
+    if (~S)
       Z <= 1;
     else
       Z <= 0;
@@ -25,7 +25,8 @@ module somador(
     else
       N <= 0;
     // Check if S is even or odd. If S is even, then P := 1, otherwise P := 0
-    if (S[0] == 0)
+    // this is done by checking the LSB is or 0 or 1.
+    if (~S[0])
       P <= 1;
     else
       P <= 0;
