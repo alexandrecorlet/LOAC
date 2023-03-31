e3e062233b5580f9d5bdf36f8920e97ac4a9a7f8
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2023-03-31 17:16:27 -0300

    Refactor somador.sv code, made it more efficient

--- R01/problema02/somador.sv
+++ R01/problema02/somador.sv
@@ -4,30 +4,22 @@
  * (c) Alexandre B. Corlet, 119210883
  */
 
-parameter NUM_BITS = 8;
+parameter NUM_BITS = 8;	// Define a constant which represent the maximum number of bits
 
 module somador(
-  input signed [NUM_BITS-1:0] A, B,
-  output signed [NUM_BITS-1:0] S,
+  input logic signed [NUM_BITS-1:0] A, B,
+  output logic signed [NUM_BITS-1:0] S,
   output logic Z, N, P
   );
   always_comb begin
     // Compute sum of A and B and store it on S
     S <= A + B;
     // Check if S == 0, if S == 0, then Z := 1, otherwise set Z := 0
-    if (S == 0)
-      Z <= 1;
-    else
-      Z <= 0;
+    Z <= S == 0;
     // Check if S < 0. If S < 0, then N := 1, otherwise N := 0
-    if (S < 0)
-      N <= 1;
-    else
-      N <= 0;
+    N <= S < N;
     // Check if S is even or odd. If S is even, then P := 1, otherwise P := 0
-    if (S[0] == 0)
-      P <= 1;
-    else
-      P <= 0;
+    // this is done by checking the LSB is or 0 or 1.
+    P <= S[0] == 0;
   end
 endmodule
