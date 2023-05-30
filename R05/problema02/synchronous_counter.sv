568d2fa0370b0192e4c9c0aa6a8e57c1a19a4463
Alexandre Corlet <alexandre.santos@ccc.ufcg.edu.br>
2023-05-30 10:51:03 -0300

    Solve problema 02

---
+++ R05/problema02/synchronous_counter.sv
@@ -0,0 +1,31 @@
+/*
+ * LOAC 2022.
+ * Roteiro 05 - Problema 02
+ * @author Alexandre B. Corlet dos Santos
+ * @matricula 119210883
+ */
+
+parameter NUM_BITS = 4;
+
+module synchronous_counter(
+  input logic clk, reset, load,
+  input logic [NUM_BITS-1:0] data_in,
+  output logic [NUM_BITS-1:0] Saida
+);
+  
+  always_ff @(posedge reset or posedge clk) begin
+  
+    if (reset)
+      Saida <= 1;		
+    else if (load)
+      Saida <= data_in;
+    else begin
+      if (Saida == 8) 
+        Saida <= 1;												
+      else 
+        Saida <= Saida << 1;
+    end
+  
+  end
+  
+endmodule
