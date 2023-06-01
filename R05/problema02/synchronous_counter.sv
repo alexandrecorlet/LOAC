028c9c0c7a70233a09a87f3fd39acb41a0b9f075
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2023-05-31 22:36:39 -0300

    Document code and solve bug

--- R05/problema02/synchronous_counter.sv
+++ R05/problema02/synchronous_counter.sv
@@ -5,26 +5,22 @@
  * @matricula 119210883
  */
 
-parameter NUM_BITS = 4;
+parameter NUM_BITS = 4;                     // Set maximum number of bits
 
 module synchronous_counter(
-  input logic clk, reset, load,
-  input logic [NUM_BITS-1:0] data_in,
-  output logic [NUM_BITS-1:0] Saida
+  input logic clk, reset,             		// Input variables for clock and reset
+  output logic [NUM_BITS-1:0] Saida         // Output (Counter)
 );
-  
+ 
   always_ff @(posedge clk) begin
   
     if (reset)
-      Saida <= 1;		
-    else if (load)
-      Saida <= data_in;
-    else begin
-      if (Saida == 8) 
+      Saida <= 1;                           // Reset output value to 1
+    else
+      if (Saida[NUM_BITS-1]) // If counter == 8, then counter := 1 and restart
         Saida <= 1;												
-      else 
+      else // Increment counter multiplying it by 2, this is done by shifting the bits to the left
         Saida <= Saida << 1;
-    end
   
   end
   
